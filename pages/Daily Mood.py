7f63c6d74956afd3599af703269b7c53c9f94a8f
Mahdi <mahdimeyghani02@gmail.com>
2024-05-06 20:51:40 +0330

    Change theme and Add daily mood page

--- pages/Daily Mood.py
+++ pages/Daily Mood.py
@@ -3,14 +3,14 @@ import time
 import moods_functions
 
 today = time.strftime("%Y/%m/%d")
-rating_list = moods_function.get_moods()
+rating_list = moods_functions.get_moods()
 
 
 def rate_mood():
     if st.session_state["mood"]:
         new_mood = f"Your mood at {today} is: {st.session_state['mood']}\n"
         rating_list.insert(0, new_mood)
-        moods_function.write_moods(rating_list)
+        moods_functions.write_moods(rating_list)
     for i in rating_list:
         st.write(i)
 
